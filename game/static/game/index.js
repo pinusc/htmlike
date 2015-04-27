c72f8ed825dad9acc11cbd6d6e34cadfc7cfad6e
pinusc <giuseppe.stelluto@gmail.com>
2015-04-27 16:21:43 +0200

    Variable refactoring.
    "h" and "w" variables were renamed in frame_height_px and frame_width_px.
    Added frame_height and _frae_width for dimension in tiles.

--- game/static/game/index.js
+++ game/static/game/index.js
@@ -1,8 +1,6 @@
-var context, h, w;
-var posx = 1, posy = 1;
+var context, frame_height_px, frame_width_px, frame_height, frame_width;
 var m = new map();
 var gdim = 13 * 4; // 12 is theo riginal image size, 4 the scaling;
-
 function handleKeys(e){
     var pl = entitiesL[0];
     switch (e.which) {
@@ -31,14 +29,16 @@ $(document).ready(function (){
     // Set canvas height and width based on container dimensions
     var width = $("#main").css("width");
     var height = $("#main").css("height");
-    w = parseInt(width.substring(0, width.length -2)); // numerical values
-    h = parseInt(height.substring(0, height.length -2));
+    frame_width_px = parseInt(width.substring(0, width.length - 2)); // numerical values
+    frame_height_px = parseInt(height.substring(0, height.length - 2));
+    frame_height = frame_height_px / gdim;
+    frame_width = frame_width_px / gdim;
     $canvas.attr("width", width);
     $canvas.attr("height", height);
 
     context = $canvas[0].getContext('2d');
 
-    $(document.body).on('keydown', handleKeys);
     m.loadMap()
     m.render();
+    $(document.body).on('keydown', handleKeys);
 });
