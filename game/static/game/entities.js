0eaed800569f26301932a386068144b1243e2fae
pinusc <giuseppe.stelluto@gmail.com>
2015-04-27 16:17:12 +0200

    Now can't move outside map
    Function move(x, y) now checks for the coordinates to be in the map, and
    won't move the player if theu aren't.
    Also posx and posy variables now can be accessed as object members

--- game/static/game/entities.js
+++ game/static/game/entities.js
@@ -6,14 +6,20 @@ dirt.src = '/static/game/dirt.png';
 var entitiesL = [new entity(greeny)];
 
 function entity(img){
-    var posx, posy;
+    this.posy = 0;
+    this.posx = 0;
     this.image = img;
 };
 
 entity.prototype.move = function(x, y) {
-    console.log(x + " : " + y)
-    if(!window.m.level[1][x][y]){
-        posx = x;
-        posy = y;
+    //If X and Y are in the margins
+    if( x >= 0 &&
+        x < m.level[0].length &&
+        y >= 0 &&
+        y < m.level[0][0].length){
+        if(!window.m.level[1][x][y]){
+            this.posx = x;
+            this.posy = y;
+        }
     }
 };
