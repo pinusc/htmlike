2c815c76cae561b77389eb716deee834270cea2b
pinusc <giuseppe.stelluto@gmail.com>
2015-05-01 15:54:41 +0200

    Fix Image() call
    Image path can't be passed as argument. Fixed that

--- game/static/game/tile.js
+++ game/static/game/tile.js
@@ -2,6 +2,7 @@ function Tile(image){
     if (image instanceof Image){
         this.image = image;
     } else {
-        this.image = new Image(image)
+        this.image = new Image()
+        this.image.src = image;
     }
 }
