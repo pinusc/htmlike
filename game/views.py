dac7ab28a1dc90d5047a21e909efe6b8af8acf3b
pinusc <giuseppe.stelluto@gmail.com>
2015-05-08 11:57:31 +0200

    Remove hardcoded level and unused logs

--- game/views.py
+++ game/views.py
@@ -3,7 +3,6 @@ from django_ajax.decorators import ajax
 
 # Create your views here.
 def index(request):
-    print("called index")
     return render(request, 'game/index.html')
 
 
@@ -13,23 +12,6 @@ def level(request):
     Return a JSON level, for now it is loaded from a file, in future it will be
     generated
     """
-    print("called level")
-    t = [  ## x and y are reversed
-        "  #####        ",
-        "  #   #        ",
-        "  #   #        ",
-        "  ## ##        ",
-        "               ",
-        "               ",
-        "               ",
-        "               ",
-        "               ",
-        "               ",
-        "               ",
-        "               ",
-        "               ",
-        "               ",
-        "               ",]
     with open("game/static/game/level.txt", 'r') as f:
         t = f.readlines()
     return {'t': t}
