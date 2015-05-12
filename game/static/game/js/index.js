ec950e410c297011f22b0a941707d30bae57888f
pinusc <giuseppe.stelluto@gmail.com>
2015-05-12 18:24:06 +0200

    Add basic touch support
    Added basic touch support in update() function, so that the game is
    playable even on touch devices such as smartphones

--- game/static/game/js/index.js
+++ game/static/game/js/index.js
@@ -2,7 +2,7 @@ var context, frame_height_px, frame_width_px, frame_height, frame_width;
 var game;
 var m = new map();
 var gdim = 13 * 4; // 12 is the original image size, 4 the scaling;
-var debug = false;
+var toDebug = true, dbl = [];
 function handleKeys(e){
     /**
     * Takes care of any key event.
@@ -33,7 +33,7 @@ function handleKeys(e){
         break;
 
         case 51:
-        debug = !debug;
+        toDebug = !toDebug;
         break;
 
         default:
@@ -58,7 +58,7 @@ $(document).ready(function (){
     frame_width = frame_width_px / gdim;
 
     // TODO see if Phaser can handle the size itself
-    game = new Phaser.Game(frame_width_px, frame_height_px, Phaser.CANVAS, 'main',
+    game = new Phaser.Game(frame_width_px, frame_height_px, Phaser.AUTO, 'main',
         { preload: preload, create: create, update: update, render: render });
 });
 
@@ -72,14 +72,35 @@ function create() {
 }
 
 function update() {
+    player = m.player;
+    if(game.input.pointer1.isDown){
+        if (Math.floor(game.input.pointer1.x/(this.game.width/3)) === 0) {
+            player.move(player.posx - 1, player.posy);
+        }
 
+        else if (Math.floor(game.input.pointer1.x/(this.game.width/3)) === 2) {
+            player.move(player.posx + 1, player.posy);
+        }
+        else if (Math.floor(game.input.pointer1.y/(this.game.height/3)) === 0) {
+            player.move(player.posx, player.posy - 1);
+        }
+
+        else if (Math.floor(game.input.pointer1.y/(this.game.height/3)) === 2) {
+            player.move(player.posx, player.posy + 1);
+        }
+    }
 }
 
 function render() {
-    if(debug){
-        game.debug.text("DEBUG:", 10, 10);
-        game.debug.text("playerHP: " + m.player.hp, 10, 25);
+    if(toDebug){
+        console.log("toDebug");
+        debug("playerHP: " + m.player.hp, "#000000");
+        if(m.entitiesL[0]){
+            debug("PrincessHP: " + m.entitiesL[0].hp);
+        }
+        renderDebug();
     }
+    game.debug.pointer(game.input.pointer1);
 }
 
 function preload() {
@@ -90,4 +111,27 @@ function preload() {
     /* tiles */
     game.load.image('grass', '/static/game/assets/grass.png');
     game.load.image('dirt', '/static/game/assets/dirt.png');
+}
+
+
+function renderDebug(){
+    /**
+    * Tell Phaser to actually render the debug info pushed through debug()
+    */
+    var x = 15;
+    game.debug.text("DEBUG:", 10, x);
+    for(var i = 0; i < dbl.length; i++){
+        x += 15;
+        var m = dbl[i];
+        game.debug.text(m.text, 10, x, m.color);
+    }
+    dbl = [];  // clear debug list
+}
+
+function debug(text, color){
+    /**
+    * Add a debug text
+    */
+
+    dbl.push({text: text, color: color});
 }
