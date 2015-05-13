67fb789e3acd6481c9f21666ebbd8b7d53d72304
pinusc <giuseppe.stelluto@gmail.com>
2015-05-13 14:34:49 +0200

    Move input functions to new file
    Created file input.js for handling user input.
    Fixed a minor bug that involved not moving entities when using
    touchscreen

---
+++ game/static/game/js/input.js
@@ -0,0 +1,74 @@
+function handleKeys(e){
+    /**
+    * Takes care of any key event.
+    * If an arrow is pressed, moves the player in the given direction
+    * If Enter is pressed, heals the player
+    * F3: toggle debug info
+    */
+    player = m.player;  //much less ugly than repeating it
+    switch (e.which) {
+        case 37: // left arrow
+        player.move(player.posx - 1, player.posy);
+        break;
+
+        case 38: // up arrow
+        player.move(player.posx, player.posy - 1);
+        break;
+
+        case 39: // right arrow
+        player.move(player.posx + 1, player.posy);
+        break;
+
+        case 40: // down arrow
+        player.move(player.posx, player.posy + 1);
+        break;
+
+        case 13: //enter
+        player.heal(1);
+        break;
+
+        case 51:
+        toDebug = !toDebug;
+        break;
+
+        default:
+        console.log("Key pressed: " + e.which);
+        break;
+    }
+
+    for (var i = 0; i < m.entitiesL.length; i++) {
+        m.entitiesL[i].act();
+    }
+}
+
+function handleTap(){
+    player = m.player;
+    if (Math.floor(game.input.pointer1.x/(this.game.width/3)) === 0) {
+        player.move(player.posx - 1, player.posy);
+    }
+
+    else if (Math.floor(game.input.pointer1.x/(this.game.width/3)) === 2) {
+        player.move(player.posx + 1, player.posy);
+    }
+    else if (Math.floor(game.input.pointer1.y/(this.game.height/3)) === 0) {
+        player.move(player.posx, player.posy - 1);
+    }
+
+    else if (Math.floor(game.input.pointer1.y/(this.game.height/3)) === 2) {
+        player.move(player.posx, player.posy + 1);
+    } else {
+        gofull();
+    }
+
+    for (var i = 0; i < m.entitiesL.length; i++) {
+        m.entitiesL[i].act();
+    }
+}
+
+function gofull() {
+    if (game.scale.isFullScreen){
+        game.scale.stopFullScreen();
+    } else {
+        game.scale.startFullScreen(false);
+    }
+}
