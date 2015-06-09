ee912ccf4585b9dfb1901e090671adbb7a4d840d
pinusc <giuseppe.stelluto@gmail.com>
2015-06-09 15:39:39 +0200

    Add items
    Weapon is asuperclass for all Weapons.
    HealPotion1 was created.
    IronSword was created.

--- static/js/items.js
+++ static/js/items.js
@@ -4,3 +4,19 @@ Items.Potion = function(image, x, y){
 	Item.call(this, image, x, y);
 }
 Items.Potion.prototype = Item.prototype;
+
+Items.Weapon = function(image, x, y){
+	Item.call(this, image, x, y);
+}
+Items.Weapon.prototype = Item.prototype;
+
+
+Items.HealPotion1 = function(x, y){
+	Items.Potion.call(this, 'potion', x, y);
+}
+Items.HealPotion1.prototype = Items.Potion.prototype;
+
+Items.IronSword = function(x, y){
+	Items.Weapon.call(this, 'iron_sword', x, y);
+}
+Items.IronSword.prototype = Items.Weapon.prototype;
