4beade01942d2d16c44960e93820f61847ba784b
pinusc <giuseppe.stelluto@gmail.com>
2015-10-29 18:36:34 +0100

    Fix Potion class
    In the level there were 3 potions of generic class "Potion", now they
    are "HealPotion1"

--- static/js/map.js
+++ static/js/map.js
@@ -33,9 +33,9 @@
       }
       this.player = new Player('greeny', this);
       this.entitiesL[0] = new Entity('princess', 12, 2, this);
-      this.itemsL.push(new Items.Potion('potion', 20, 20, this));
-      this.itemsL.push(new Items.Potion('potion', 20, 22, this));
-      this.itemsL.push(new Items.Potion('potion', 20, 17, this));
+      this.itemsL.push(new Items.HealPotion1(20, 20, this));
+      this.itemsL.push(new Items.HealPotion1(20, 22, this));
+      this.itemsL.push(new Items.HealPotion1(20, 17, this));
       this.itemsL.push(new Items.Sword(22, 20, this));
       this.player.inventory.updateShowInventory(this.box.game, this.box.properties.gdim);
       this.flag = 1;
